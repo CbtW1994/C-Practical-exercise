e9990a4f9944db46bb4a6797e0612e62fee7bd09
Charlie Webb <Charles_Webb1994@hotmail.com>
2020-10-12 18:11:12 +0100

    Add project files.

---
+++ GoodEnergyPracticalExercise/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GoodEnergyPracticalExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
